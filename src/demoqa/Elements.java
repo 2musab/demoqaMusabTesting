ac7b8f9f310817e7326dfbe8b56569dfa037810c
2musab <musab_Sadoon@hotmail.com>
2024-09-10 23:43:39 +0300

    update

--- src/demoqa/Elements.java
+++ src/demoqa/Elements.java
@@ -1,11 +1,17 @@
 package demoqa;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,10 +22,12 @@ public class Elements extends Parameters {
 	public void mySetup() {
 
 		driver.get("https://demoqa.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 	}
 
-	@Test(priority = 1, enabled = false)
+	@Test(enabled = false)
 	public void textArea() {
 
 		driver.get("https://demoqa.com/text-box");
@@ -39,9 +47,14 @@ public class Elements extends Parameters {
 		WebElement submit = driver.findElement(By.id("submit"));
 		submit.click();
 
+		WebElement name = driver.findElement(By.id("name"));
+
+		boolean actaulText = name.getText().contains("Name");
+
+		Assert.assertEquals(actaulText, true);
 	}
 
-	@Test(priority = 2)
+	@Test(enabled = false)
 	public void checkBox() throws InterruptedException {
 
 		driver.get("https://demoqa.com/checkbox");
@@ -58,13 +71,78 @@ public class Elements extends Parameters {
 		WebElement officeCheck = driver
 				.findElement(By.cssSelector("label[for='tree-node-office'] span[class='rct-checkbox']"));
 		officeCheck.click();
-		
-		Thread.sleep(5000);
-		
+
 		boolean actualResult = driver.findElement(By.id("result")).getText().contains("selected");
-		
+
 		System.out.println(actualResult);
 		Assert.assertEquals(actualResult, true);
+	}
+
+	@Test(enabled = false)
+	public void webtables() throws InterruptedException {
+
+		driver.get("https://demoqa.com/webtables");
+
+		WebElement add = driver.findElement(By.id("addNewRecordButton"));
+		add.click();
+
+		WebElement firstName = driver.findElement(By.id("firstName"));
+		firstName.sendKeys(mName);
+
+		WebElement LastName = driver.findElement(By.id("lastName"));
+		LastName.sendKeys(surName);
+
+		WebElement email = driver.findElement(By.id("userEmail"));
+		email.sendKeys(mName + surName + "@gmail.com");
+
+		WebElement age = driver.findElement(By.id("age"));
+		age.sendKeys("36");
+
+		WebElement salary = driver.findElement(By.id("salary"));
+		salary.sendKeys("350");
+
+		WebElement dept = driver.findElement(By.id("department"));
+		dept.sendKeys("IT");
+
+		WebElement submit = driver.findElement(By.id("submit"));
+		submit.click();
+
+		System.out.println("Name : " + mName + " " + surName);
+		System.out.println("Email : " + mName + surName + "@gmail.com");
+
+		WebElement container = driver
+				.findElement(By.cssSelector("div[class='rt-tr -even'] div[class='action-buttons']"));
+		WebElement delete = container.findElement(By.id("delete-record-2"));
+		delete.click();
+
+	}
+
+	@Test
+	public void doubleClick() {
+
+		driver.get("https://demoqa.com/buttons");
+
+		Actions action = new Actions(driver);
+
+		WebElement doubleClick = driver.findElement(By.id("doubleClickBtn"));
+		action.doubleClick(doubleClick).perform();
+
+		WebElement rightClick = driver.findElement(By.id("rightClickBtn"));
+		action.contextClick(rightClick).perform();
+	}
+
+	@Test(enabled = false)
+	public void radio() {
+
+		driver.get("https://demoqa.com/radio-button");
+
+		WebElement impressive = driver.findElement(By.cssSelector("label[for='impressiveRadio']"));
+		impressive.click();
+
+		String actualRadio = driver.findElement(By.cssSelector(".mt-3")).getText();
+		String expectedRadio = "You have selected Impressive";
+
+		Assert.assertEquals(actualRadio, expectedRadio);
 	}
 
 }
